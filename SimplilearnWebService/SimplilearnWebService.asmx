1babdbb8fb10bbc0d7fa43ad3dbf7d0e4854858e
Daniel Onguene <danielonguene@gmail.com>
2023-07-09 17:06:03 -0400

    Ajoutez des fichiers projet.

---
+++ SimplilearnWebService/SimplilearnWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="SimplilearnWebService.asmx.cs" Class="SimplilearnWebService.SimplilearnWebService" %>
---
+++ SimplilearnWebService/SimplilearnWebService.asmx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SimplilearnWebService
+{
+    /// <summary>
+    /// Description résumée de SimplilearnWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SimplilearnWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(MessageName ="Addition de deux nombres")]
+        public int Sum(int x, int y)
+        {
+            return x+y;
+        }
+
+        [WebMethod(MessageName = "Difference de deux nombres")]
+        public int Difference(int x, int y)
+        {
+            return x - y;
+        }
+
+        [WebMethod(MessageName = "Produit de deux nombres")]
+        public int Produit(int x, int y)
+        {
+            return x * y;
+        }
+
+        [WebMethod(MessageName = "Division de deux nombres")]
+        public int Division(int x, int y)
+        {
+            return x % y;
+        }
+    }
+}
